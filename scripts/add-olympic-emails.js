b37956d2b092370082ed2df92dfaf3ecfbe7e39c
Joe Auty <joe@datasprocket.com>
2021-09-13 21:07:06 -0700

    attach headers to CSV

--- scripts/add-olympic-emails.js
+++ scripts/add-olympic-emails.js
@@ -1,15 +1,19 @@
 var fs = require('fs'); 
 const csv = require('csv-parser');
 var results = [];
+var headers = [];
 
 fs.createReadStream('../source-data/athletes.csv')
   .pipe(csv())
+  .on('headers', (data) => {
+    headers.push(data)
+  })
   .on('data', (data) => {
     data['Email'] = data['Name'].replace(/\s/g,'').toLowerCase() + '@olympics.com';
     results.push(data)
   })
   .on('end', () => {
-    var finalCSV = "";
+    var finalCSV = headers.join(',') + "\n";
     results.map(r => {
       finalCSV += "\"" + r['Name'] + "\",\"" + r['NOC'] + "\",\"" + r['Discipline'] + "\",\"" + r['Email'] + "\"\n";
     })
